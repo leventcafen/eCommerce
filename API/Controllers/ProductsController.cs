e162af3894d622c51290d713793789774fb1e1dc
Levent Cafen <leventcafen@Levent-MacBook-Pro.local>
2021-05-11 23:47:50 +0300

    Initial commit

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,17 +13,16 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepository _productRepository;
+        public ProductsController(IProductRepository productRepository)
         {
-            _context = context;
-
+            _productRepository = productRepository;
         }
 
         [HttpGet]
-        public async Task< ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products=await _context.Products.ToListAsync();
+            var products = await _productRepository.GetProductAsync();
             return Ok(products);
         }
 
@@ -30,7 +30,17 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _productRepository.GetProductByIdAsync(id);
+        }
+        [HttpGet("brands")]
+        public async Task<ActionResult<ProductBrand>> GetProductBrands()
+        {
+            return Ok( await _productRepository.GetProductBrandsAsync());
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<Product>> GetProductTypes()
+        {
+            return Ok (await _productRepository.GetProductTypesAsync());
         }
     }
 }
